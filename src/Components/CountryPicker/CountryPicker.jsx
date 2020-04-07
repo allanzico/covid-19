0465cf8531ed90c486c3b8d5fc28d02017df140b
Allan <akanyijuka@gmail.com>
2020-04-07 23:15:40 +0200

    Add select

--- src/Components/CountryPicker/CountryPicker.jsx
+++ src/Components/CountryPicker/CountryPicker.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Form } from 'react-bootstrap';
+import styles from './CountryPicker.module.css';
+import cx from 'classnames';
+import Select from 'react-select';
+import ReactFlagsSelect from 'react-flags-select';
+import 'react-flags-select/css/react-flags-select.css';
+import 'react-flags-select/scss/react-flags-select.scss'
 
 const CountryPicker = () => {
-    return (
-        <Select>
 
-        </Select>
+    return (
+        <div>
+            <Form>
+                <Form.Label>
+                    <Form.Control className={cx(styles.selectpicker, styles.countrypicker, styles.formControl)} as="select" data-flag="true" custom>
+                        <option value="global">Global</option>
+                    </Form.Control>
+                </Form.Label>
+            </Form>
+
+        </div>
     )
 }
 
